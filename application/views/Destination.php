bff0e136a07dfc8f62116a66c2ad778b256bbaa9
Tiffany Thompson <tdthompson899@gmail.com>
2017-03-10 00:27:26 -0600

    Fixed db error - added travel db to ubuntu server and added background colors to login, dashboard, destination and add pages

--- application/views/Destination.php
+++ application/views/Destination.php
@@ -4,20 +4,22 @@
   <meta name="description" content="Shows the details of plan selected">
   <title>Destination Information</title>
   <body>
-    <a href="/Dashboards">Home</a>
-    <a href="/Dashboards/end">Logout</a>
+    <div class="show_div">
+      <a href="/Dashboards">Home</a>
+      <a href="/Dashboards/end">Logout</a>
 
-    <h2><?php echo $show['destination']; ?></h2>
+      <h2><?php echo $show['destination']; ?></h2>
 
-    <h3>Planned By: <?= $show['name']?></h3>
-    <h3>Description: <?= $show['description']?></h3>
-    <h3>Travel Date From: <?= $show['start_date']?></h3>
-    <h3>Travel Date To: <?= $show['end_date']?></h3>
+      <h3>Planned By: <?= $show['name']?></h3>
+      <h3>Description: <?= $show['description']?></h3>
+      <h3>Travel Date From: <?= $show['start_date']?></h3>
+      <h3>Travel Date To: <?= $show['end_date']?></h3>
 
-    <h2>Other Users' joining the trip:</h2>
-    <?php
-        foreach($joining as $values):
-          echo $values['name']. "</br>";
-    endforeach;  ?>
+      <h2>Other Users' joining the trip:</h2>
+      <?php
+          foreach($joining as $values):
+            echo $values['name']. "</br>";
+      endforeach;  ?>
+    </div>
   </body>
 </html>
--- application/views/add.php
+++ application/views/add.php
@@ -8,18 +8,20 @@
     <link rel="stylesheet" href="/assets/stylesheets/style.css">
   </head>
   <body>
-    <h2 class="add">Add a Plan</h2>
-    <a href="/Dashboards">Home</a>
-    <a href="/Dashboards/end">Logout</a>
-    <form method="post" action="/Dashboards/add/<?php echo $this->session->userdata['user_data']['id']; ?>">
-      <?= $this->session->flashdata('add_errors'); ?>
-      <?= $this->session->flashdata('date_errors'); ?>
-      <?= $this->session->flashdata('end_errors'); ?>
-      <input type="text" name="dest" placeholder="Destination">
-      <input type="text" name="descr" placeholder="Description">
-      <label for="start_date">Travel Start-Date: </label><input type="date" name="start">
-      <label for="end_date">Travel End-Date: </label><input type="date" name="end">
-      <input type="submit" value="Add Plan">
-    </form>
+    <div class="add_div">
+      <h2 class="add">Add a Plan</h2>
+      <a href="/Dashboards">Home</a>
+      <a href="/Dashboards/end">Logout</a>
+      <form method="post" action="/Dashboards/add/<?php echo $this->session->userdata['user_data']['id']; ?>">
+        <?= $this->session->flashdata('add_errors'); ?>
+        <?= $this->session->flashdata('date_errors'); ?>
+        <?= $this->session->flashdata('end_errors'); ?>
+        <input type="text" name="dest" placeholder="Destination">
+        <input type="text" name="descr" placeholder="Description">
+        <label for="start_date">Travel Start-Date: </label><input type="date" name="start">
+        <label for="end_date">Travel End-Date: </label><input type="date" name="end">
+        <input type="submit" value="Add Plan">
+      </form>
+    </div>
   </body>
 </html>
